978aac1d659401f399025ef604c3b3212bffd1e7
RootLEE2 <2rootcom4@gmail.com>
2022-08-01 11:57:40 +0900

    feat: game data class in data

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DataFrame;
+
+public class GameData : MonoBehaviour
+{
+    public bool newGame;
+    public SettingData settingData;
+    public CharacterData characterData;
+    public PlayData playData;
+
+    public GameData() {
+        newGame = true;
+        settingData = new SettingData();
+        characterData = new CharacterData();
+        playData = new PlayData();
+    }
+
+    public string ToJson() {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void LoadFromJson(string a_Json) {
+        JsonUtility.FromJsonOverwrite(a_Json, this);
+    }
+}
