2ec4f9d171a0780320ca1b7580db2e41ac616f39
hch3245 <hch3245@naver.com>
2022-08-01 12:12:27 +0900

    feat: transition-v1.0

---
+++ Assets/Scripts/Transition/Transition.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public static Transition instance;
+
+    [Header("Const Variables")]
+    private const float blinkTime = 1.0f;
+
+    [Header("Object Variables")]
+    [SerializeField] private GameObject filterObject;
+    private Color filterColor;
+
+    [Header("Variables")]
+    private string destSceneName;
+    private AsyncOperation op;
+
+
+    void Awake(){
+        instance = this;
+        filterColor = filterObject.GetComponent<Image>().color;
+    }
+
+    public void SceneTransition(string destScene){
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        destSceneName = destScene;
+        filterObject.SetActive(true);
+        StartCoroutine(SceneTransitionCor());
+    }
+
+    private IEnumerator SceneTransitionCor(){
+        AsyncLoad();
+        yield return StartCoroutine(Fade(true));
+        while(!CheckIfDataLoaded()){
+            yield return null;
+        }
+        ActivateScene();
+        yield break;
+    }
+
+    private void AsyncLoad(){
+        op = SceneManager.LoadSceneAsync(destSceneName);
+        op.allowSceneActivation = false;
+    }
+
+    private IEnumerator Fade(bool isFadeIn){
+        float step_timer = 0.0f;
+        while(step_timer <= blinkTime){
+            yield return null;
+            step_timer += Time.unscaledDeltaTime;
+            float rate = step_timer / blinkTime;
+            rate = Mathf.Min(rate, 1.0f);
+            rate = Mathf.Sin(rate * Mathf.PI / 2.0f);
+            filterColor.a = isFadeIn ? Mathf.Lerp(0.0f, 1.0f, rate) : Mathf.Lerp(1.0f, 0.0f, rate);
+            filterObject.GetComponent<Image>().color = filterColor;
+        }
+        
+        if(!isFadeIn){
+            PrepareScene();
+        }
+    }
+
+    private bool CheckIfDataLoaded(){
+        // if(!isDataLoaded){
+        //     return false;
+        // }
+        return true;
+    }
+
+    private void ActivateScene(){
+        op.allowSceneActivation = true;
+        if(destSceneName == "Puzzle"){
+            // PuzzleManager.AwakePuzzle();
+        }
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1){
+        if(arg0.name == destSceneName){
+            StartCoroutine(Fade(false));
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+    }
+
+    private void PrepareScene(){
+        filterObject.SetActive(false);
+    }
+}
