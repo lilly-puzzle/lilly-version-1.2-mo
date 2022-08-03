8481afc36b58e2c5bcbe53e459d32ee4f63e8baa
hch3245 <hch3245@naver.com>
2022-08-03 11:31:51 +0900

    fix: resetgame don't reset setting data

--- Assets/Scripts/Data/DataManager.cs
+++ Assets/Scripts/Data/DataManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using DataFrame;
+
 public class DataManager : MonoBehaviour
 {
     public static DataManager instance;
@@ -13,6 +15,8 @@ public class DataManager : MonoBehaviour
 
         gameData = new GameData();
         LoadDataFromJson();
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
     // If game is quited..
@@ -43,7 +47,9 @@ public class DataManager : MonoBehaviour
 
     // Reset
     public void ResetGame() {
-        gameData = new GameData();
+        gameData.newGame = true;
+        gameData.characterData = new CharacterData();
+        gameData.playData = new PlayData();
     }
 
     // Setter
