f9851b4edee22ac30f7d3b68d94521b17ede0f06
RootLEE2 <2rootcom4@gmail.com>
2022-08-07 13:45:51 +0900

    feat: smooth move func for RectTransform in class

---
+++ Assets/Scripts/Class/Coroutine/RectMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectMove : MonoBehaviour
+{
+    [Header("Transform Variable")]
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // move coroutine
+    protected IEnumerator SmoothMove(float a_duration, Vector3 a_targetPosition) {
+        Vector3 startPos  = rectTransform.anchoredPosition;
+        Vector3 finalPos = a_targetPosition;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < a_duration) {
+            float rate = elapsedTime / a_duration;
+            rate = Mathf.Min(rate, 1.0f);
+            rate = Mathf.Sin(rate * Mathf.PI / 2.0f);
+
+            rectTransform.anchoredPosition = Vector3.Lerp(startPos, finalPos, rate);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
