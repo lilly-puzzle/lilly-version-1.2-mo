528970b7de93d365a06f684602190d2232444118
RootLEE2 <2rootcom4@gmail.com>
2022-08-07 20:31:34 +0900

    fix: zoom or not item list in default data

--- Assets/Scripts/Data/DefaultData.cs
+++ Assets/Scripts/Data/DefaultData.cs
@@ -1,6 +1,13 @@
+using System.Collections.Generic;
+
 public class DefaultData
 {
     public const int NUM_OF_FLOOR = 4;
     public const int MAX_NUM_OF_PUZ_PER_FLOOR = 16;
     public const int MAX_NUM_OF_ITEMS_PER_PUZ = 32;
+
+    public static readonly List<List<int>> ZOOM_OR_NOT = new List<List<int>>
+    {
+        new List<int> { }
+    };
 }
