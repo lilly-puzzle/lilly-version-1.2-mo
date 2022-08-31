84b6b5fa3e3e668891289084f43b609c21d3bb39
RootLEE2 <2rootcom4@gmail.com>
2022-08-31 20:03:13 +0900

    feat: drag event of switch in tv

--- Assets/Scripts/Puzzle/Floor1/TVfrequency/TVSwitchDrag.cs
+++ Assets/Scripts/Puzzle/Floor1/TVfrequency/TVSwitchDrag.cs
@@ -6,22 +6,57 @@ public class TVSwitchDrag : SimpleOneTouch
 {
     [Header("Variables")]
     private bool isDragging;
-    private int lastSwitchIdx;
+    private List<float> switchLoc;
+    private List<float> moveBoundary;
+    private int switchIdx;
+
+    [Header("Script Variables")]
+    private TVSwitchMove moveScript;
+
+    private void Awake() {
+        moveScript = gameObject.GetComponent<TVSwitchMove>();
+    }
 
     private void LateUpdate() {
         if (!isDragging) return;
 
-        if (touchPhase == touchPhaseMoved) {
-            // TODO: col로 하위 object가 있는지 확인
-        } else if (touchPhase == touchPhaseEnded) {
-            // TODO: 가장 최근에 선택된 스위치 위치로 이동
+        if (touchPhase == touchPhaseEnded) {
+            switch(ray.y) {
+                case float val when (switchLoc[0] <= val && val <= moveBoundary[0]): {
+                    switchIdx = 0;
+                    break;
+                }
+                case float val when (moveBoundary[0] < val && val < moveBoundary[1]): {
+                    switchIdx = 1;
+                    break;
+                }
+                case float val when (moveBoundary[1] <= val && val <= switchLoc[2]): {
+                    switchIdx = 2;
+                    break;
+                }
+                default: {
+                    switchIdx = -1;
+                    break;
+                }
+            }
+
+            Vector3 temp = transform.position;
+            temp.y = switchLoc[switchIdx];
+            moveScript.MoveToPosition(temp);
         }
     }
 
     private void FixedUpdate() {
         if (!isDragging) return;
 
-        // TODO: object의 position 변경 (touch의 y좌표만 사용)
+        float yCoor = ray.y;
+
+        if (yCoor < moveBoundary[0]) yCoor = moveBoundary[0];
+        else if (moveBoundary[1] < yCoor) yCoor = moveBoundary[1];
+
+        Vector3 temp = transform.position;
+        temp.y = yCoor;
+        transform.position = temp;
     }
 
     protected override void FuncWhenTouchBegan() {
