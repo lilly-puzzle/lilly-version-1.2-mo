628db7d5c6bb085b592fa71aba9286ace6c75156
hch3245 <hch3245@naver.com>
2022-09-04 15:47:32 +0900

    feat: TouchBlock v1.0

---
+++ Assets/Scripts/Puzzle/TouchBlockManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchBlockManager : MonoBehaviour
+{
+    public static TouchBlockManager instance;
+    [SerializeField] private GameObject BlockObject;
+    private GameObject touchAbleUI;
+    private Vector3 savePos;
+
+
+    void Awake(){
+        instance = this;
+        BlockObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 0, -5);
+    }
+
+    public void TouchBlock(GameObject touchAbleUI_){
+        BlockObject.SetActive(true);
+        touchAbleUI = touchAbleUI_;
+        savePos = touchAbleUI.GetComponent<RectTransform>().anchoredPosition;
+        touchAbleUI.GetComponent<RectTransform>().anchoredPosition = new Vector3(savePos.x, savePos.y, -6);
+    }
+
+    public void TouchBlockEnd(){
+        BlockObject.SetActive(false);
+        touchAbleUI.GetComponent<RectTransform>().anchoredPosition = savePos;
+    }
+}
